fa0e4b5a387fdd404048d1b534682fe781c5a7e1
alafyte <xleb.xlebnyj@mail.ru>
2024-05-06 20:43:59 +0300

    Package.json has been updated

--- PizzaItaliana/upload.ts
+++ PizzaItaliana/upload.ts
@@ -6,7 +6,7 @@ const upload = multer({ storage: storage });
 
 const storage_disk = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, 'public', 'images'))
+        cb(null, path.join('public', 'images'))
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + file.originalname;
